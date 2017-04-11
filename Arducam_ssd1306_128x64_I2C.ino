c60251b0667bbdf8390f789f7753349d906c4470
supprot <support@arducam.com>
2017-04-11 12:07:04 +0800

    Add files via upload

---
+++ Arducam_ssd1306_128x64_I2C.ino
@@ -0,0 +1,57 @@
+
+// Import required libraries
+#include <ArducamSSD1306.h>    // Modification of Adafruit_SSD1306 for ESP8266 compatibility
+#include <Adafruit_GFX.h>   // Needs a little change in original Adafruit library (See README.txt file)
+#include <Wire.h>           // For I2C comm, but needed for not getting compile error
+
+/*
+HardWare I2C pins
+A4   SCL
+A5  SDA
+*/
+
+// Pin definitions
+#define OLED_RESET  16  // Pin 15 -RESET digital signal
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+ArducamSSD1306 display(OLED_RESET); // FOR I2C
+
+
+void setup(void)
+{
+	// Start Serial
+	Serial.begin(115200);
+
+  // SSD1306 Init
+  display.begin();  // Switch OLED
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(20,20);
+  display.println("Hello, Arducam!");
+  display.display();
+
+
+
+}
+
+
+void loop() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
